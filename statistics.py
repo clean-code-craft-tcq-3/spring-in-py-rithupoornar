bc409568665f627de5555972fb4a21de0f470b3b
Rithupoorna <99022079+rithupoornar@users.noreply.github.com>
2022-03-04 13:42:02 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,13 +1,13 @@
 
 def calculateStats(numbers):
   computedNumbers = {}
-    try:
-        computedNumbers["avg"] = sum(numbers)/len(numbers)
-        computedNumbers["max"] = max(numbers)
-        computedNumbers["min"] = min(numbers)
-    except Exception as e:
-        computedNumbers["avg"] = float('nan')
-        computedNumbers["max"] = float('nan')
-        computedNumbers["min"] = float('nan')
-    
-    return computedNumbers
+  try:
+    computedNumbers["avg"] = sum(numbers)/len(numbers)
+    computedNumbers["max"] = max(numbers)
+    computedNumbers["min"] = min(numbers)
+  except Exception as e:
+    computedNumbers["avg"] = float('nan')
+    computedNumbers["max"] = float('nan')
+    computedNumbers["min"] = float('nan')
+
+  return computedNumbers
